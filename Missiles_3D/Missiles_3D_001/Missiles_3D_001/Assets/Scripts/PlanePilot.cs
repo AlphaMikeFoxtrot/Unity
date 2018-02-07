66f554ca0b7d42ed83de68add85ef7888a233cf0
ANONYMOUS <iamcicada3301@gmail.com>
2018-02-07 15:09:54 +0530

    init summary

---
+++ Missiles_3D/Missiles_3D_001/Missiles_3D_001/Assets/Scripts/PlanePilot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanePilot : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("script added to : " + transform.name);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += transform.forward * Time.deltaTime * speed;
+
+		speed -= transform.forward.y * Time.deltaTime * 50.0f;
+
+		if (speed < 35.00f) {
+			speed = 35.00f;
+		}
+
+		if (Input.GetKey ("space")) {
+		
+			speed -= 10 * Time.deltaTime;
+		
+		} else {
+		
+			// speed = 100f;
+		
+		}
+
+		transform.Rotate (Input.GetAxis ("Vertical") * 1.5f, 0.0f, -Input.GetAxis ("Horizontal") * 1.5f);
+
+		float bottomTerrain = Terrain.activeTerrain.SampleHeight (transform.position);
+		if (bottomTerrain - 10 > transform.position.y) {
+		
+			transform.position = new Vector3 (transform.position.x, bottomTerrain, transform.position.z);
+		
+		}
+
+	}
+}
